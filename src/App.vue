1755fed2b915692af18bcef43c085c1937216209
Saba <saba.sadegh67@gmail.com>
2023-04-18 16:53:23 +0430

    ordering ads in diffrent type

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <template>
-  <div class="app"><JobList :jobs="jobs" /></div>
+  <div class="app">
+    <header>
+      <div class="order">
+        <button @click="handleClick('title')">order by title</button>
+        <button @click="handleClick('salary')">order by salary</button>
+        <button @click="handleClick('location')">order by location</button>
+      </div>
+    </header>
+
+    <JobList :jobs="jobs" :order="order" />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from "vue";
 import JobList from "./components/JobList.vue";
 import Job from "./types/jobs";
+import orderType from "./types/ordertype";
 
 export default defineComponent({
   name: "App",
@@ -17,9 +28,30 @@ export default defineComponent({
       { title: "plumber", location: "natanz", salary: 2000, id: "3" },
       { title: "prison guard", location: "karaj", salary: 4500, id: "4" },
     ]);
-    return { jobs };
+    const order = ref<orderType>("title");
+    const handleClick = (term: orderType) => {
+      order.value = term;
+    };
+    return { jobs, handleClick, order };
   },
 });
 </script>
 
-<style></style>
+<style>
+header {
+  text-align: center;
+}
+header .order {
+  margin-top: 20px;
+}
+button {
+  margin: 0 10px;
+  color: #1195c9;
+  border: 3px solid #1195c9;
+  background: #d5f0ff;
+  padding: 8px 16px;
+  border-radius: 4px;
+  cursor: pointer;
+  font-weight: bold;
+}
+</style>
--- src/components/JobList.vue
+++ src/components/JobList.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="job-list">
+    <p>ordered by {{ order }}</p>
     <ul>
       <li v-for="job in jobs" :key="job.id">
         <h2>{{ job.title }} in {{ job.location }}</h2>
@@ -17,12 +18,17 @@
 <script lang="ts">
 import { defineComponent, PropType } from "vue";
 import Job from "@/types/jobs";
+import orderType from "@/types/ordertype";
 
 export default defineComponent({
   props: {
     jobs: {
       required: true,
       type: Array as PropType<Job[]>,
+    },
+    order: {
+      required: true,
+      type: String as PropType<orderType>,
     },
   },
 });
