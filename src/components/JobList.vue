5c935be07de99928bff635eda4d4aa81616f317e
Saba <saba.sadegh67@gmail.com>
2023-04-18 17:11:24 +0430

    add sorting feature to project

--- src/components/JobList.vue
+++ src/components/JobList.vue
@@ -2,7 +2,7 @@
   <div class="job-list">
     <p>ordered by {{ order }}</p>
     <ul>
-      <li v-for="job in jobs" :key="job.id">
+      <li v-for="job in orderedJobs" :key="job.id">
         <h2>{{ job.title }} in {{ job.location }}</h2>
         <div class="salary">{{ job.salary }} toman</div>
         <div class="description">
@@ -16,7 +16,7 @@
   </div>
 </template>
 <script lang="ts">
-import { defineComponent, PropType } from "vue";
+import { computed, defineComponent, PropType } from "vue";
 import Job from "@/types/jobs";
 import orderType from "@/types/ordertype";
 
@@ -30,6 +30,14 @@ export default defineComponent({
       required: true,
       type: String as PropType<orderType>,
     },
+  },
+  setup(props) {
+    const orderedJobs = computed(() => {
+      return [...props.jobs].sort((a: Job, b: Job) => {
+        return a[props.order] > b[props.order] ? 1 : -1;
+      });
+    });
+    return { orderedJobs };
   },
 });
 </script>
